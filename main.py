dc0aed99f18d6d12d751f5bd3345485436fde9ff
Sumeet Rana <sumeetsinh28@gmail.com>
2024-03-21 01:00:09 +0400

    feat: path parameters

--- main.py
+++ main.py
@@ -4,4 +4,16 @@ app = FastAPI()
 
 @app.get("/")
 def index():
-    return 'heyy' 
+    return {
+        'data': 'blog list'
+    } 
+
+@app.get("/blog/{id}")
+def show(id: int):
+    return {
+        'data': id
+    }
+
+@app.get('/blog/{id}/comments')
+def comments(id):
+    return {'data': {'1', '2'}}
